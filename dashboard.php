356582786b3ff28b1d6ee9db5d3e3641656ec68b
Istiak60 <istisquare@gmail.com>
2021-06-24 22:05:00 +0600

    24-6-2021

--- dashboard.php
+++ dashboard.php
@@ -26,6 +26,12 @@ session_start();
     <!-- <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet"> -->
 
   <!-- <link rel="stylesheet" href=" https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css"> -->
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+<!-- JavaScript Bundle with Popper -->
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+
 </head>
 <body>
     <section class="header">
@@ -39,13 +45,29 @@ session_start();
             <ul>
               <li><a href="">ABOUT</a></li>
               <li><a href="">CONTACT</a></li>
-              <li><a href="">CATAGORIES</a></li>
+             
               <li><a href="">PROFILE</a></li>
               <li><a href="logout.php">LOG OUT</a></li>
+               <li><a class="btn btn-secondary dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-bs-toggle="dropdown" aria-expanded="false" style ="background-color:rgba(0,0,0,0.01);border: 0px">CATEGORIES
+  </a>
+
+  <ul class="dropdown-menu dropdown-menu-dark" aria-labelledby="dropdownMenuLink">
+    <li><a class="dropdown-item"  href="#">Bangla Literature</a></li>
+    <li><a class="dropdown-item" href="#">Nobels</a></li>
+    <li><a class="dropdown-item" href="#">Poems</a></li>
+    <li><a class="dropdown-item" href="#">Story</a></li>
+  </ul></li>
+  
             </ul>
           </div>
+          
+          
           <i class="fa fa-bars" onclick="showmenu()"></i>
         </nav>
+  
+
+
+
   
         <div class="text-box">
           <h1>Book Cafee</h1>
