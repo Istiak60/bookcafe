80e97b6f01eae34e658a5e53947caad82c03bb74
Istiak60 <istisquare@gmail.com>
2021-07-26 10:24:00 +0600

    dropdown correction

--- adlog.php
+++ adlog.php
@@ -7,25 +7,52 @@ $dbname = "bookcafe1_db";  #database name
 #connection string
 $conn = mysqli_connect($localhost,$dbusername,$dbpassword,$dbname);
  
-if($_SERVER['REQUEST_METHOD']== "POST")
- {
-
- $t=$_POST['book_name'];
- $p =$_POST['price'];
- $q =$_POST['quantity'];
- 
-
-
-$sql ="insert into orders (book_name,price,quantity)  values ('$t','$p','$q') ";
-
-
-    if(mysqli_query($conn,$sql)){
- 
-        
-    }
-    else{
-        echo "Error";
-    }
+if(isset($_POST["add_to_cart"]))
+{
+	if(isset($_SESSION["shopping_cart"]))
+	{
+		$item_array_id = array_column($_SESSION["shopping_cart"], "item_id");
+		if(!in_array($_GET["id"], $item_array_id))
+		{
+			$count = count($_SESSION["shopping_cart"]);
+			$item_array = array(
+				'item_id'			=>	$_GET["id"],
+				'item_name'			=>	$_POST["hidden_name"],
+				'item_price'		=>	$_POST["hidden_price"],
+				'item_quantity'		=>	$_POST["quantity"]
+			);
+			$_SESSION["shopping_cart"][$count] = $item_array;
+		}
+		else
+		{
+			echo '<script>alert("Item Already Added")</script>';
+		}
+	}
+	else
+	{
+		$item_array = array(
+			'item_id'			=>	$_GET["id"],
+			'item_name'			=>	$_POST["hidden_name"],
+			'item_price'		=>	$_POST["hidden_price"],
+			'item_quantity'		=>	$_POST["quantity"]
+		);
+		$_SESSION["shopping_cart"][0] = $item_array;
+	}
+}
+if(isset($_GET["action"]))
+{
+	if($_GET["action"] == "delete")
+	{
+		foreach($_SESSION["shopping_cart"] as $keys => $values)
+		{
+			if($values["item_id"] == $_GET["id"])
+			{
+				unset($_SESSION["shopping_cart"][$keys]);
+				echo '<script>alert("Item Removed")</script>';
+				echo '<script>window.location="index.php"</script>';
+			}
+		}
+	}
 }
  
  
@@ -70,6 +97,7 @@ $sql ="insert into orders (book_name,price,quantity)  values ('$t','$p','$q') ";
             integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM"
             crossorigin="anonymous"
         ></script>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js"></script>
 
 <style>
 img{height:70px;width:50px;}
@@ -167,11 +195,13 @@ img{height:70px;width:50px;}
         echo "<th>";  echo "Quantity";    echo "</th>";
         echo "<th>";  echo "Add to cart";    echo "</th>";
         echo"</tr>";
+       if(mysqli_num_rows($res)>0)
+       {
         while($row= mysqli_fetch_array($res))
-        { 
+        
              {    
                  ?>
-            <form action=""method="POST" ecntype="multipart/form-data">
+            <form method="post" action="index.php?action=add&id=<?php echo $row["id"]; ?>" ecntype="multipart/form-data">
             <tr>     
                <td > <?php echo '<img src="data:image;base64,'.base64_encode($row['image']).' " >';?>    </td>
        
@@ -183,7 +213,7 @@ img{height:70px;width:50px;}
          <td><input type="text" id="country" name="price" value=<?php echo $row['price'];?> readonly><br><br></td>
          <td > <?php echo $row['description'];?>    </td>
          <td > <?php echo '<input  type="number" name="quantity" style="width: 70px; height: 20px:">';?>    </td>
-         <td > <?php echo'<input type="submit" name="submit" value="Add to cart" class="btn btn-success">';?>    </td>
+         <td > <?php echo'<input type="submit" name="add_to_cart" value="Add to cart" class="btn btn-success">';?>    </td>
        
        
        
@@ -193,6 +223,7 @@ img{height:70px;width:50px;}
  
 
         </tr>
+        </form>
         <?php
             } 
          
@@ -202,9 +233,50 @@ img{height:70px;width:50px;}
       
         echo "</table>";
   ?>
+<div style="clear:both"></div>
+			<br />
+			<h3>Order Details</h3>
+			<div class="table-responsive">
+				<table class="table table-bordered">
+					<tr>
+						<th width="40%">Item Name</th>
+						<th width="10%">Quantity</th>
+						<th width="20%">Price</th>
+						<th width="15%">Total</th>
+						<th width="5%">Action</th>
+					</tr>
+					<?php
+					if(!empty($_SESSION["shopping_cart"]))
+					{
+						$total = 0;
+						foreach($_SESSION["shopping_cart"] as $keys => $values)
+						{
+					?>
+					<tr>
+						<td><?php echo $values["item_name"]; ?></td>
+						<td><?php echo $values["item_quantity"]; ?></td>
+						<td>$ <?php echo $values["item_price"]; ?></td>
+						<td>$ <?php echo number_format($values["item_quantity"] * $values["item_price"], 2);?></td>
+						<td><a href="index.php?action=delete&id=<?php echo $values["item_id"]; ?>"><span class="text-danger">Remove</span></a></td>
+					</tr>
+					<?php
+							$total = $total + ($values["item_quantity"] * $values["item_price"]);
+						}
+					?>
+					<tr>
+						<td colspan="3" align="right">Total</td>
+						<td align="right">$ <?php echo number_format($total, 2); ?></td>
+						<td></td>
+					</tr>
+					<?php
+					}
+					?>
+						
+				</table>
+			</div>
+		</div>
 
-
-    </form>
+        </div>
 
         <!-- Footer -->
         <section class="footer">
